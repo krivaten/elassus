7017dc205c9cd1f2eb6bac3ba8b0504e9c530eaf
Kris Van Houten <kristopher.vanhouten@q2ebanking.com>
2017-03-03 09:34:12 -0500

    Build setup for adding examples

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,35 +1,44 @@
-var gulp = require('gulp'),
-  sass = require('gulp-sass'),
-  cleanCSS = require('gulp-clean-css'),
-  rename = require('gulp-rename'),
-  connect = require('gulp-connect');
-
-gulp.task('styles', function() {
-  gulp.src('elassus.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./css/'))
-    .pipe(cleanCSS({ compatibility: 'ie8' }))
-    .pipe(rename({ suffix: '.min' }))
-    .pipe(gulp.dest('./css/'))
-    .pipe(connect.reload());
-});
-
-gulp.task('html', function () {
-  gulp.src('index.html')
-    .pipe(connect.reload());
-});
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var pug = require('gulp-pug');
+var prettify = require('gulp-html-prettify');
+var cleanCSS = require('gulp-clean-css');
+var rename = require('gulp-rename');
+var connect = require('gulp-connect');
 
 gulp.task('connect', function() {
-  connect.server({
-    root: '',
-    livereload: true
-  });
+    connect.server({
+        root: './html',
+        livereload: {
+            port: 35730
+        }
+    });
+});
+
+gulp.task('styles', function() {
+    gulp.src('./elassus.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./css/'))
+        .pipe(gulp.dest('./html/css/'))
+        .pipe(cleanCSS({ compatibility: 'ie8' }))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest('./css/'))
+        .pipe(gulp.dest('./html/css/'))
+        .pipe(connect.reload());
+});
+
+gulp.task('pug', function buildHTML() {
+    return gulp.src('pug/*.pug')
+        .pipe(pug())
+        .pipe(prettify({ indent_char: ' ', indent_size: 4 }))
+        .pipe(gulp.dest('./html'))
+        .pipe(connect.reload());
 });
 
 gulp.task('watch', function() {
-  gulp.watch('scss/**/*.scss', ['styles']);
-  gulp.watch('index.html', ['html']);
+    gulp.watch(['./scss/**/*.scss'], ['styles']);
+    gulp.watch(['./pug/**/*.pug'], ['pug']);
 });
 
 //Watch task
-gulp.task('default', ['styles', 'connect', 'watch']);
+gulp.task('default', ['styles', 'pug', 'connect', 'watch']);
