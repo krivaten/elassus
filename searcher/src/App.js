206a2e78abfc483ecbc12a2452a90f32ebfa9335
Kris Van Houten <kristopher.vanhouten@q2ebanking.com>
2017-05-29 23:24:23 -0400

    Add ability to link to reference

--- searcher/src/App.js
+++ searcher/src/App.js
@@ -2,38 +2,48 @@ import React, { Component } from 'react';
 import Section from './components/Section';
 import cssJson from './css.json';
 
-function debounce(fn, delay) {
-  var timer = null;
-  return function () {
-    var context = this, args = arguments;
-    clearTimeout(timer);
-    timer = setTimeout(function () {
-      fn.apply(context, args);
-    }, delay);
-  };
-}
+const getParams = query => {
+    if (!query) {
+        return {};
+    }
+
+    return (/^[?#]/.test(query) ? query.slice(1) : query)
+        .split('&')
+        .reduce((params, param) => {
+            let [key, value] = param.split('=');
+            params[key] = value ? decodeURIComponent(value.replace(/\+/g, ' ')) : '';
+            return params;
+        }, {});
+};
 
 class App extends Component {
     constructor() {
         super();
 
-        this.search = debounce(this.search, 200);
+        this.updateQuery = this.updateQuery.bind(this);
+        this.search = this.search.bind(this);
+
+        let queryParams = getParams(window.location.search);
 
         this.state = {
             results: {},
-            query: ''
+            query: queryParams.s
         };
     }
 
     componentWillMount() {
-        this.search();
+        this.search(this.state.query);
+    }
+
+    updateQuery(query) {
+        this.setState({ query });
+        this.search(query);
     }
 
     search(query) {
         let results;
 
         if (query) {
-            this.setState({ query });
             results = cssJson.filter((item) => {
                 let title = item.title ? item.title.toLowerCase() : null;
 
@@ -51,16 +61,16 @@ class App extends Component {
 
         return (
             <div>
-                <div className="p(2) br(2) bg-c(primary)">
+                <div className="p(2) bdrad(2) bgc(primary)">
                     <label htmlFor="search" className="sr">Search</label>
-                    <input type="text" id="search" className="d(b) w(100%) br(2) m(0) p(2) bd(0) h3" placeholder="Padding, Color, Flex, etc." onChange={ (e) => this.search(e.target.value) } />
+                    <input type="text" id="search" className="d(b) w(100%) bdrad(2) m(0) p(2) bd(0) h3" placeholder="Padding, Color, Flex, etc." value={query} onChange={(e) => this.updateQuery(e.target.value)} />
                 </div>
 
-                <ul className="ls(n) bg-c(white)" aria-live="polite" aria-atomic="true">
+                <ul className="ls(n) bgc(white)" aria-live="polite" aria-atomic="true">
                     {
                         Object
                             .keys(this.state.results)
-                            .map((key) => <Section key={ key } section={ this.state.results[key] } />)
+                            .map((key) => <Section key={key} section={this.state.results[key]} />)
                     }
                 </ul>
             </div>
